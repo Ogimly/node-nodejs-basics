95d269a1478a2c322232f0846dd223694586be71
ogimly <ogimly@gmail.com>
2022-12-04 04:18:38 +0500

    feat: add streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { Transform, pipeline } from 'stream';
+import { EOL } from 'os';
+
 const transform = async () => {
-    // Write your code here 
+  const revert = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(
+        null,
+        chunk.toString().replace(EOL, '').split('').reverse().join('') + EOL
+      );
+    },
+  });
+
+  pipeline(process.stdin, revert, process.stdout, (error) => {
+    if (error) {
+      stderr.write(`error on transform: ${error}`);
+    }
+  });
+
+  console.log('Type anything to console:\n');
 };
 
 await transform();
