116d65f0e7534d0f9d7efdd3455c146328c5c544
ogimly <ogimly@gmail.com>
2022-12-04 02:53:53 +0500

    feat: add list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import fsPromises from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { getFullName } from '../utils/utils.js';
+
+const errorMessage = 'FS operation failed';
+
 const list = async () => {
-    // Write your code here 
+  const metaUrl = fileURLToPath(import.meta.url);
+  const folderName = 'files';
+  const fullName = getFullName(metaUrl, folderName);
+
+  try {
+    const files = await fsPromises.readdir(fullName, { withFileTypes: true });
+    files.forEach((file) =>
+      file.isFile()
+        ? console.log(`file: ${file.name}`)
+        : console.log(`folder: ${file.name}`)
+    );
+
+    console.log(`Done`);
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
 };
 
 await list();
