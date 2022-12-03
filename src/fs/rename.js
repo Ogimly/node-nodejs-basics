ba90d06e2933e9880942d717792d9907896fd6c1
ogimly <ogimly@gmail.com>
2022-12-04 02:38:05 +0500

    feat: add rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import fsPromises from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { getFullName } from '../utils/utils.js';
+
+const errorMessage = 'FS operation failed';
+
 const rename = async () => {
-    // Write your code here 
+  const metaUrl = fileURLToPath(import.meta.url);
+  const fileName = 'wrongFilename.txt';
+  const folderName = 'files';
+  const fileNameNew = 'properFilename.md';
+  const fullName = getFullName(metaUrl, folderName, fileName);
+  const fullNameNew = getFullName(metaUrl, folderName, fileNameNew);
+
+  try {
+    await fsPromises.writeFile(fullNameNew, '', { flag: 'wx' });
+    await fsPromises.rename(fullName, fullNameNew);
+    console.log(`"${fileName}" renamed to "${fileNameNew}"`);
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
 };
 
 await rename();
