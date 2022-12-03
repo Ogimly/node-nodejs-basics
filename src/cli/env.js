c7d2572b3a6810e7925ff3e0ef9707c267ffe4e8
ogimly <ogimly@gmail.com>
2022-12-04 04:40:16 +0500

    feat: add cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  Object.entries(process.env).forEach(([key, value]) => {
+    if (key.indexOf('RSS_') !== -1) {
+      console.log(`${key} = ${value}`);
+    }
+  });
 };
 
 parseEnv();
