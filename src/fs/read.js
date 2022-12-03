0153790cc933eb80ae01feb4de646a516d4489ee
ogimly <ogimly@gmail.com>
2022-12-04 03:08:20 +0500

    feat: add read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import fsPromises from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { getFullName } from '../utils/utils.js';
+
+const errorMessage = 'FS operation failed';
+
 const read = async () => {
-    // Write your code here 
+  const metaUrl = fileURLToPath(import.meta.url);
+  const fileName = 'fileToRead.txt';
+  const folderName = 'files';
+  const fullName = getFullName(metaUrl, folderName, fileName);
+
+  try {
+    const content = (await fsPromises.readFile(fullName)).toString();
+    console.log(`Content of "${fileName}":`);
+    console.log(content);
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
 };
 
 await read();
