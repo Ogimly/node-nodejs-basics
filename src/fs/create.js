64006dab60046bbe1c95a144f0c1ef0d008884fe
ogimly <ogimly@gmail.com>
2022-12-04 00:40:02 +0500

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
-const create = async () => {
-    // Write your code here 
+import { writeFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+export const create = async () => {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const fileName = 'fresh.txt';
+  const folderName = 'files';
+  const fullName = path.join(__dirname, folderName, fileName);
+
+  const content = 'I am fresh and young';
+  const errorMessage = 'FS operation failed';
+
+  try {
+    await writeFile(fullName, content, { flag: 'wx' });
+    console.log(`"${fileName}" created`);
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
 };
 
-await create();
+create();
