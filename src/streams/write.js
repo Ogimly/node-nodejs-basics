3ca3dbd4bf7a644d157749a86045bb52f4335f64
ogimly <ogimly@gmail.com>
2022-12-04 04:06:40 +0500

    feat: add streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import { stdout, stdin } from 'process';
+import { fileURLToPath } from 'url';
+import { getFullName } from '../utils/utils.js';
+
+const errorMessage = 'FS operation failed';
+
 const write = async () => {
-    // Write your code here 
+  const metaUrl = fileURLToPath(import.meta.url);
+  const fileName = 'fileToWrite.txt';
+  const folderName = 'files';
+  const fullName = getFullName(metaUrl, folderName, fileName);
+
+  try {
+    const streamWrite = fs.createWriteStream(fullName, 'utf-8');
+    process.stdin.pipe(streamWrite);
+
+    stdout.write('Enter text to write to file (press Ctrl+C to cancel):');
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
 };
 
 await write();
