5fe2698b802c7e8346fb264972e64a96a3362266
ogimly <ogimly@gmail.com>
2022-12-04 02:44:49 +0500

    feat: add delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import fsPromises from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { getFullName } from '../utils/utils.js';
+
+const errorMessage = 'FS operation failed';
+
 const remove = async () => {
-    // Write your code here 
+  const metaUrl = fileURLToPath(import.meta.url);
+  const fileName = 'fileToRemove.txt';
+  const folderName = 'files';
+  const fullName = getFullName(metaUrl, folderName, fileName);
+
+  try {
+    await fsPromises.rm(fullName);
+    console.log(`"${fileName}" deleted`);
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
 };
 
 await remove();
