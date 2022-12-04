33c32a740f90c23d33146b9bd753bb5e74c9fd5a
ogimly <ogimly@gmail.com>
2022-12-04 15:31:22 +0500

    feat: add compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,32 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { fileURLToPath } from 'url';
+import { getFullName } from '../utils/utils.js';
+
 const compress = async () => {
-    // Write your code here 
+  const metaUrl = fileURLToPath(import.meta.url);
+  const fileName = 'fileToCompress.txt';
+  const folderName = 'files';
+  const fileNameZip = 'archive.gz';
+  const fullName = getFullName(metaUrl, folderName, fileName);
+  const fullNameZip = getFullName(metaUrl, folderName, fileNameZip);
+
+  try {
+    const readStream = createReadStream(fullName);
+    const writeStream = createWriteStream(fullNameZip);
+    const Gzip = createGzip();
+
+    pipeline(readStream, Gzip, writeStream, (error) => {
+      if (error) {
+        stderr.write(`error on compressing: ${error}`);
+      }
+    });
+
+    console.log(`"${fileName}" compressed to "${fileNameZip}"`);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await compress();
