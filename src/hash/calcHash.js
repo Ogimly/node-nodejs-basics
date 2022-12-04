3e328279b5a6ace55dfb3896d226da1a8dc2661c
ogimly <ogimly@gmail.com>
2022-12-04 05:37:25 +0500

    feat: add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import fsPromises from 'fs/promises';
+import { createHash } from 'crypto';
+import { fileURLToPath } from 'url';
+import { getFullName } from '../utils/utils.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const metaUrl = fileURLToPath(import.meta.url);
+  const fileName = 'fileToCalculateHashFor.txt';
+  const folderName = 'files';
+  const fullName = getFullName(metaUrl, folderName, fileName);
+  console.log(fullName);
+
+  try {
+    const content = await fsPromises.readFile(fullName);
+    const hash = createHash('sha256').update(content).digest('hex');
+    console.log('Hash: ' + hash);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await calculateHash();
