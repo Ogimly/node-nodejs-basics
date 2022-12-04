35f862600f745ffae403535f81a69a956ea1b8c8
ogimly <ogimly@gmail.com>
2022-12-04 17:35:07 +0500

    feat: add worker.js and main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { fileURLToPath } from 'url';
+import { getFullName } from '../utils/utils.js';
+
 const performCalculations = async () => {
-    // Write your code here
+  const metaUrl = fileURLToPath(import.meta.url);
+  const fileName = 'worker.js';
+  const fullName = getFullName(metaUrl, fileName);
+
+  const CPUs = cpus();
+  let counter = 10;
+
+  const resultsOfWorkers = await Promise.allSettled(
+    CPUs.map(() => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(fullName, {
+          workerData: counter,
+        });
+        counter += 1;
+
+        worker.on('message', (res) => resolve(res));
+        worker.on('error', (res) => reject(res));
+      });
+    })
+  );
+
+  const results = resultsOfWorkers.map((res) =>
+    res.status === 'fulfilled'
+      ? { status: 'resolved', data: res.value }
+      : { status: 'error', data: null }
+  );
+
+  console.log(results);
 };
 
 await performCalculations();
