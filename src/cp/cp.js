a70edc3a08ad0a7b4d842878603177ec991100dc
ogimly <ogimly@gmail.com>
2022-12-04 19:57:49 +0500

    feat: add cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import { getFullName } from '../utils/utils.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const metaUrl = fileURLToPath(import.meta.url);
+  const fileName = 'script.js';
+  const folderName = 'files';
+  const fullName = getFullName(metaUrl, folderName, fileName);
+
+  const childProcess = fork(fullName, [...args], {
+    stdio: [process.stdin, process.stdout, 'ipc'],
+  });
 };
 
-spawnChildProcess();
+spawnChildProcess(['1', '2', '3']);
